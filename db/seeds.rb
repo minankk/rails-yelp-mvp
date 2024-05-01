27aba1e567d7127dfff0a7e8e28821bf58c6d6be
minan kahai <minankahai@gmail.com>
2024-05-01 16:28:25 +0100

    ylp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.create(name: "Bancone", address: "Address 1", phone_number: "123-456-7890", category: "italian")
+Restaurant.create(name: "Nobu", address: "Address 2", phone_number: "123-456-7890", category: "japanese")
+Restaurant.create(name: "LPM", address: "Address 3", phone_number: "123-456-7890", category: "french")
+Restaurant.create(name: "Hutong", address: "Address 4", phone_number: "123-456-7890", category: "chinese")
+Restaurant.create(name: "Balans", address: "Address 5", phone_number: "123-456-7890", category: "belgian")
